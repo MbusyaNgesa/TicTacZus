dcac5299d82c7a9c25fe11c78cb0dd3b172af143
MbusyaNgesa <theengesa@gmail.com>
2024-11-21 17:08:15 +0300

    Setting up history

---
+++ src/components/History.jsx
@@ -0,0 +1,13 @@
+//Here we are revisiting previous moves in the game
+export const history = [
+  [null, null, null, null, null, null, null, null, null],
+  ["X", null, null, null, null, null, null, null, null],
+  ["X", "0", null, null, null, null, null, null, null],
+  ["X", "0", "X", null, null, null, null, null, null],
+  ["X", "0", "X", "O", null, null, null, null, null],
+  ["X", "0", "X", "O", "X", null, null, null, null],
+  ["X", "0", "X", "O", "X", "O", null, null, null],
+  ["X", "0", "X", "O", "X", "O", "X", null, null],
+  ["X", "0", "X", "O", "X", "O", "X", "O", null],
+  ["X", "0", "X", "O", "X", "O", "X", "O", "X"],
+];
